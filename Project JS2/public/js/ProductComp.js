c95c3fb64db7711d74d0586fac1a7518cc185d1b
Yuri Kim <yurikimkrk@gmail.com>
2020-12-18 13:52:20 +0700

    les7

--- Project JS2/public/js/ProductComp.js
+++ Project JS2/public/js/ProductComp.js
@@ -1,22 +1,21 @@
 Vue.component('products', {
-    data(){
+    data() {
         return {
-            catalogUrl: '/catalogData.json',
             products: [],
             filtered: [],
             imgCatalog: 'https://placehold.it/200x150',
         }
     },
     methods: {
-        filter(value){
+        filter(value) {
             let regexp = new RegExp(value, 'i');
             this.filtered = this.products.filter(el => regexp.test(el.product_name));
         }
     },
-    mounted(){
-        this.$parent.getJson(`${API + this.catalogUrl}`)
+    mounted() {
+        this.$parent.getJson('/api/products')
             .then(data => {
-                for(let el of data){
+                for (let el of data) {
                     this.products.push(el);
                     this.filtered.push(el);
                 }
@@ -24,16 +23,17 @@ Vue.component('products', {
     },
     template: `
         <div class="products">
-            <product ref="refref" v-for="item of filtered" :key="item.id_product" :img="imgCatalog" :product="item"></product>
+            <product v-for="item of filtered" :key="item.id_product" :img="imgCatalog" :product="item"></product>
         </div>
     `
 });
+
 Vue.component('product', {
     props: ['product', 'img'],
     data() {
-      return {
-          basketAPI: this.$root.$refs.basket,
-      };
+        return {
+            cartAPI: this.$root.$refs.cart,
+        };
     },
 
     template: `
@@ -42,7 +42,7 @@ Vue.component('product', {
                 <div class="desc">
                     <h3>{{product.product_name}}</h3>
                     <p>{{product.price}}₽</p>
-                    <button class="buy-btn" @click="basketAPI.addProduct(product)">Купить</button>
+                    <button class="buy-btn" @click="cartAPI.addProduct(product)">Купить</button>
                 </div>
             </div>
     `
